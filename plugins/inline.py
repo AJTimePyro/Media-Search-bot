393a041706fa804acf2c0c9247d1c023f61359e0
Alι_Aryαɴ Tecн <85696485+AliAryanTech@users.noreply.github.com>
2021-12-10 12:53:20 +0500

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -72,13 +72,15 @@ async def answer(bot, query):
 
 def get_reply_markup(username, query):
     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
-    buttons = [[
-        InlineKeyboardButton('Search Again 🔍', switch_inline_query_current_chat=query),
-        InlineKeyboardButton('Share Bot 🔥', url=url),
-    ]],
-     [
+    buttons = [
+        [
+           InlineKeyboardButton('Search Again 🔍', switch_inline_query_current_chat=query),
+           InlineKeyboardButton('Share Bot 🔥', url=url),
+        ],
+
+        [
            InlineKeyboardButton('Updates Channel 🔔 ', url='https://t.me/AccessAdminBot_AL'),
-           InlineKeyboardButton('Subscribe To Help 🤖 ', url='https://t.me/AnimeListUp'),
+           InlineKeyboardButton('Subscribe To help 🤖 ', url='https://t.me/AnimeListUp'),
         ],
     ]
     return InlineKeyboardMarkup(buttons)
